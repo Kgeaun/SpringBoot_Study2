0e67a515d66f860df96364bf9d4772c03767ee64
김가은 <kge02160@gmail.com>
2024-02-14 18:45:13 +0900

    Initial commit

---
+++ src/test/java/com/example/springboot_study2/SpringBootStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_study2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
