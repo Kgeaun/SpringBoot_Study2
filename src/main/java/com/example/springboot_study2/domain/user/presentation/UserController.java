b722cb33140b29aa7875a9e9f3cc4741b6aa0996
Kgeaun <kge02160@gmail.com>
2024-02-18 04:33:44 +0900

    🌧 :: UserController 추가

---
+++ src/main/java/com/example/springboot_study2/domain/user/presentation/UserController.java
@@ -0,0 +1,22 @@
+package com.example.springboot_study2.domain.user.presentation;
+
+import com.example.springboot_study2.domain.user.presentation.dto.request.UserSignUpRequest;
+import com.example.springboot_study2.domain.user.service.UserSignUpService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserSignUpService userSignUpService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public void signUp(@RequestBody @Valid UserSignUpRequest request) {
+        userSignUpService.signUp(request);
+    }
+}
