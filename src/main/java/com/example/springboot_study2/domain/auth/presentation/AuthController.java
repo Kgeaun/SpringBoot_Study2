5cd4b5b1e2737dad79f039a2588bf2fedbb61df6
Kgeaun <kge02160@gmail.com>
2024-02-18 11:03:36 +0900

    🌧 :: AuthController 추가

---
+++ src/main/java/com/example/springboot_study2/domain/auth/presentation/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.springboot_study2.domain.auth.presentation;
+
+import com.example.springboot_study2.domain.auth.presentation.dto.request.SignInRequest;
+import com.example.springboot_study2.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.springboot_study2.domain.auth.service.ReissueService;
+import com.example.springboot_study2.domain.auth.service.SignInService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final SignInService loginService;
+    private final ReissueService reissueService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/token")
+    public TokenResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return loginService.signIn(request);
+    }
+
+
+}
