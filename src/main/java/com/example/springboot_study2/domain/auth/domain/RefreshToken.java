f176056446e38aec72c82f5e19987addacfb5d9a
Kgeaun <kge02160@gmail.com>
2024-02-23 13:57:56 +0900

    ♻️ :: 어노테이션 추가

--- src/main/java/com/example/springboot_study2/domain/auth/domain/RefreshToken.java
+++ src/main/java/com/example/springboot_study2/domain/auth/domain/RefreshToken.java
@@ -1,13 +1,14 @@
 package com.example.springboot_study2.domain.auth.domain;
 
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @RedisHash
 public class RefreshToken {
